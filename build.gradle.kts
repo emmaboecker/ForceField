d04ff9f5513991822bf4f64722fe0d3998ee419b
Marcel Böcker <57072067+StckOverflw@users.noreply.github.com>
2022-01-07 14:27:10 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    id("io.papermc.paperweight.userdev") version "1.3.4-SNAPSHOT"
+}
+
+group = "org.example"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // PaperMC Dependency
+    paperDevBundle("1.18.1-R0.1-SNAPSHOT")
+
+    // KSpigot dependency
+    implementation("net.axay", "kspigot", "1.18.0")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
